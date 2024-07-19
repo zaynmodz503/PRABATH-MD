bd0eada53ff719abe41a0f9f6efeee176cdfab74
PRABATH KUMARA <106251140+prabathLK@users.noreply.github.com>
2024-07-19 19:37:01 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,6 +1,8 @@
 const envv2 = require('./config-v2')
 var GITHUB_TOKEN;
 var BOT_NUMBER;
+var SESSION_ID;
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -19,12 +21,17 @@ BOT_NUMBER = process.env.BOT_NUMBER
 }else{
 BOT_NUMBER = envv2.BOT_NUMBER
 }
+if(process.env.SESSION_ID){
+SESSION_ID = process.env.SESSION_ID
+}else{
+SESSION_ID = envv2.SESSION_ID
+}
 
 
 
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
+    SESSION_ID: SESSION_ID,    
     BOT_NUMBER:  BOT_NUMBER,
     GITHUB_USERNAME: username,
     GITHUB_AUTH_TOKEN: GITHUB_TOKEN,
